65ec2f760b0a9ec4c206d017ac2ef965edbac714
sanaatrsh <sanaatrash09@gmail.com>
2025-08-29 16:47:08 +0300

    fix image url

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'root' => public_path('app/images'),
+            'url' => env('APP_URL') . '/app/images',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
